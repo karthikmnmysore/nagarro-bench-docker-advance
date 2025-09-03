f5ca5e8e4bbc136650a0cd4f5624d3e49b354c16
karthikmnmysore <karthikmnmysore@gmail.com>
2025-09-03 18:50:58 +0530

    Add files via upload

---
+++ Monitoring-tool/Dummy.java
@@ -0,0 +1,6 @@
+public class Dummy {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Spring Boot");
+        Thread.sleep(Long.MAX_VALUE); // keeps app alive
+    }
+}
